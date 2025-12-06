cccebaa7e867534f29245e7d308a26d75fdfe6db
chxu_11 <444632064@qq.com>
2025-12-07 07:13:29 +0800

    v0.11

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,9 +7,9 @@ const config = {
   tagline: '编程教程学习平台',
   favicon: 'img/favicon.ico',
 
-  url: 'https://chxu11.github.io/mydoc/',
+  url: 'https://chxu11.github.io',
   // baseUrl  需要改成你的仓库名，如果你启用了GitHub Pages，请改成你的仓库名,默认是/
-  baseUrl: 'mydoc',
+  baseUrl: '/mydoc/',
   // 对于死链接，默认是warn，可以改成其他的选项，比如ignore,throw
   onBrokenLinks: 'warn',
 
